2798dd81c72239eaf47cec6bea2dd079bdd00654
Adrian Moya <moyaa1022@gmail.com>
2021-05-12 20:46:34 -0400

    fixed ssr bug (again)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -25,7 +25,7 @@ img {
 <h1>Finally!</h1>
 <p>After 8 years of dating, two years of engagement, and on COVID delay, we are getting married in Novemeber, 2021! We hope you will be available to join us!</p>
 
-{#if typeof window !== undefined}
+{#if typeof window !== 'undefined'}
 <div id="carouselCouplePictures" class="carousel slide" data-ride="carousel" data-interval="7000">
 	<div class="carousel-inner">
 		{#each imageURIs as imgSrc, i}
