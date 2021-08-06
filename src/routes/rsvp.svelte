d98da6f86410bc4812f258c7e9bc437bea5f9cd7
Adrian Moya <moyaa1022@gmail.com>
2021-08-05 20:11:13 -0400

    created rsvp page

---
+++ src/routes/rsvp.svelte
@@ -0,0 +1,27 @@
+<script>
+ const MAX_PARTY_SIZE = 4;
+
+</script>
+
+<h1>RSVP</h1>
+<form class='text-center'>
+        <h3>Party Members</h3>
+        <div class="container">
+            <div class="row">
+                <div class="col col-lg-6 text-left">
+                    <label class="form-label h5 col-12"> Name
+                        <input type="text" class="form-control" >
+                    </label>
+                </div>
+                <div class="col">
+                    <fieldset>
+                        <legend class="form-label h5">Dinner Preference</legend>
+                        <label><input type="radio" name="dinner-preference" value="chicken"> Chicken </label>
+                        <label><input type="radio" name="dinner-preference" value="beef"> Short Rib </label>
+                    </fieldset>
+                </div>
+            </div>
+        </div>
+
+
+</form>
