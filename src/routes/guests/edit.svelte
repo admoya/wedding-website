707b1a84b1b4ab55781fed5f72f643775368f95a
Adrian Moya <moyaa1022@gmail.com>
2021-10-26 21:56:05 +0000

    feat(export): added RSVP export

--- src/routes/guests/edit.svelte
+++ src/routes/guests/edit.svelte
@@ -27,6 +27,16 @@
     const refreshAll = async () => {
         guests = await getFullGuestList({fetch});
         remoteGuests = cloneDeep(guests);
+    };
+    let csv;
+    $: {
+        const rows = [['group name', 'group attending', 'email', 'notes', 'guest name', 'guest attending', 'food']];
+        remoteGuests.forEach(({ attending: groupAttending, email, name: groupName, notes, seats}) => {
+            seats.forEach(({ attending: guestAttending, food, name: guestName}) => {
+                rows.push([ `"${groupName}"`, groupAttending, email, `"${notes}"`, guestName, guestAttending, food]);
+            });
+        });
+        csv = `data:text/csv;charset=utf-8,${encodeURI(rows.map(r => r.join(',')).join('\n'))}`;
     }
     $: respondedGuests = guests.filter(g => g.attending !== undefined);
     $: nonRespondedGuests = guests.filter(g => g.attending === undefined);
@@ -42,6 +52,7 @@
     console.log(JSON.stringify(guests, null, 4));
     console.log(JSON.stringify(remoteGuests, null, 4));
 }}>Debug</button>
+<a href={csv} download="RSVPs.csv">Export</a>
 <Card>
     <CardHeader>
         <CardTitle>Stats</CardTitle>
