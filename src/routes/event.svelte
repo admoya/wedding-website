95af4bd130e79a29472494a391c0e7bf990e16f3
Adrian Moya <moyaa1022@gmail.com>
2021-11-05 18:38:32 +0000

    feat(event): added time to event details

--- src/routes/event.svelte
+++ src/routes/event.svelte
@@ -13,7 +13,7 @@
 
 <h1>About the Event</h1>
 
-<p>Our wedding will be on November 13th, 2021, at the beautiful <a target="_blank" href="https://g.page/hyattregencycoralgables?share">Hyatt Regency in Coral Gables</a>!</p>
+<p>Our wedding will be on November 13th, 2021, at the beautiful <a target="_blank" href="https://g.page/hyattregencycoralgables?share">Hyatt Regency in Coral Gables</a>! The ceremony will begin at 4:30PM.</p>
 <p>The ceremony will take place in the central courtyard, and the reception will be held on-site. The dress code for the event is formal.</p>
 <p>If you are interested in staying at the hotel during the weekend of the wedding, we have reserved a block of rooms just for this event! You can book yours from <a target="_blank" href="https://www.hyatt.com/en-US/group-booking/CORAL/G-HMWB">this page</a>.</p>
 <img  alt="Hyatt Regency Coral Gables" src="Hotel.webp">
